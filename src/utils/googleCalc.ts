b1de0674a46d8c3289e1e138f0890b45961864ab
4lexG <64931332+Dev4lexG@users.noreply.github.com>
2024-06-10 09:19:54 +0200

    Update googleCalc.ts

--- src/utils/googleCalc.ts
+++ src/utils/googleCalc.ts
@@ -10,7 +10,7 @@ import { gymkana as config } from '@/utils/config'
 
 const jwt = new JWT({
 	email: process.env.MAIL,
-	key: (process.env.SECRET || '') /*.replace(/\\n/g, '\n')*/,
+	key: (process.env.SECRET || '').replace(/\\n/g, '\n'),
 	scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 })
 const doc = new GoogleSpreadsheet(process.env.DOC || '', jwt)
@@ -102,27 +102,4 @@ async function insertUIDq(dat: uid) {
 	if (!updated) {
 		await sheet.addRow(data)
 	}
-
-	const rowss = await sheet.getRows()
-
-	const newData = {}
-
-	rowss.forEach((row) => {
-		const obj = row.toObject()
-
-		// @ts-expect-error
-		newData[obj.uid] = {}
-		for (const key in obj) {
-			if (obj[key]) {
-				const ke = obj[key].split(';')
-				if (ke.length === 3) {
-					// @ts-expect-error
-					newData[obj.uid][key] = { good: ke[0], errors: ke[1], tryed: ke[2] }
-				}
-				// @ts-expect-error
-				else newData[obj.uid][key] = obj[key]
-			}
-		}
-	})
-	return newData
 }
